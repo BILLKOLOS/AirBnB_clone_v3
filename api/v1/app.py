fcc5d7165d5dd28b3a96f74dc1ac0a75f190b027
BILLKOLOS <117803237+BILLKOLOS@users.noreply.github.com>
2023-07-31 13:08:19 +0300

    Update app.py
    further debugging on line 25

--- api/v1/app.py
+++ api/v1/app.py
@@ -22,7 +22,7 @@ def downtear(self):
 @app.errorhandler(404)
 def page_not_found(error):
     '''return render_template'''
-    return jsonify(error='Not found'), 404
+    return jsonify(error="Not found"), 404
 if __name__ == "__main__":
     host = getenv('HBNB_API_HOST')
     port = getenv('HBNB_API_PORT')
